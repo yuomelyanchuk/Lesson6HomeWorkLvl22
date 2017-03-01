98bf1ac481bbc084ca1ed03a27256d41f02b0c5f
Urec <Urec@DESKTOP-F6GIP85>
2017-03-02 00:24:01 +0200

    q

---
+++ src/com/gmail/yuomelyanchuk/Main.java
@@ -0,0 +1,23 @@
+package com.gmail.yuomelyanchuk;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int max = 1;
+		int tmp = 0;
+
+		for (int i = 1000; i < 10000; i++) {
+			for (int j = 1000; j < 10000; j++) {
+				StringBuilder sb = new StringBuilder();
+				tmp = i * j;
+				sb.append(tmp);
+				if (sb.toString().matches(sb.reverse().toString()) && tmp > max) {
+					max = tmp;					
+				}
+			}
+
+		}
+		System.out.println(max);
+	}
+
+}
